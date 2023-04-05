6421b7d8bd953bc8308f330520d9c045eabaa54c
Кирилл Лимонов <Limk94@mail.ru>
2023-04-05 15:12:10 +0300

    [TEAM][ASD-1111]Рефакторит код

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,17 +1,5 @@
-import styles from "./Loader.module.css";
-import "./Loader.module.css";
-
-// export enum LoaderSize {
-//   s = "20",
-//   m = "40",
-//   l = "60",
-// }
-// interface ILoaderProps {
-//   color?: string;
-//   loading?: boolean;
-//   size?: LoaderSize;
-//   className?: string;
-// }
+import styles from './Loader.module.css';
+import './Loader.module.css';
 
 export const Loader = () => {
   return <div className={styles.loader}></div>;
